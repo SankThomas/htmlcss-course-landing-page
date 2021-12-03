a2ae70ab34587ee0a6dd0a9c564048660b7da097
Thomas Sankara <thomassankara74@gmail.com>
2021-12-03 12:34:30 +0300

    style Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,23 @@
+import BuyCourse from "../buttons/BuyCourse"
+import CourseDiscussion from "../buttons/CourseDiscussion"
+
 export default function Header() {
   return (
     <>
-      <header className="bg-white shadow flex items-center justify-between p-5">
+      <header className="bg-white shadow p-5 flex justify-center flex-col sm:items-center sm:flex-row sm:justify-between 2xl:fixed 2xl:top-0 2xl:w-full">
         <div>
-          <h2 className="font-bold text-gray-800 text-4xl xl:text-6xl">
+          <h2 className="font-bold text-gray-800 text-4xl mb-5 sm:mb-0">
             Sankara
           </h2>
         </div>
 
         <nav>
-          <ul className="flex items-center justify-between">
-            <li className="mr-5">
-              <a
-                href="https://www.udemy.com/course/tsbdesigns-web-development-course/learn/?couponCode=NOVEMBER"
-                target="_blank"
-                rel="noopenner noreferrer"
-                className="bg-red-500 text-white font-bold py-2 px-6 rounded shadow border-2 border-red-500 hover:bg-transparent hover:text-red-500 duration-500 transition-all"
-              >
-                Buy the course
-              </a>
+          <ul className="grid grid-cols-1 gap-3 sm:grid-cols-2">
+            <li className="text-center">
+              <BuyCourse />
             </li>
-            <li>
-              <a
-                href="https://www.udemy.com/course/tsbdesigns-web-development-course/learn/lecture/25270392#questions"
-                target="_blank"
-                rel="noopenner noreferrer"
-                className="bg-blue-500 text-white font-bold py-2 px-6 rounded shadow border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 duration-500 transition-all"
-              >
-                Course discussion
-              </a>
+            <li className="text-center">
+              <CourseDiscussion />
             </li>
           </ul>
         </nav>
