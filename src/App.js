893a172497a3a2520a81cefb163d80ca639d9324
Thomas Sankara <thomassankara74@gmail.com>
2021-12-03 12:35:01 +0300

    render components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import Footer from "./components/Footer"
+import Features from "./components/Features"
 
 function App() {
   return (
     <main>
       <Header />
       <Hero />
+      <Features />
+      <Footer />
     </main>
   )
 }
