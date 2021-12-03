1497b7b02f2cb6f5ad4c951609897d136bab003a
Thomas Sankara <thomassankara74@gmail.com>
2021-12-03 12:33:38 +0300

    Buy Course button

---
+++ src/buttons/BuyCourse.js
@@ -0,0 +1,14 @@
+export default function BuyCourse() {
+  return (
+    <>
+      <a
+        href="https://www.udemy.com/course/tsbdesigns-web-development-course/learn/?couponCode=DECEMBER"
+        target="_blank"
+        rel="noopenner noreferrer"
+        className="bg-red-500 text-white font-bold py-2 px-6 rounded shadow border-2 border-red-500 hover:bg-transparent hover:text-red-500 duration-500 transition-all block"
+      >
+        Buy the course
+      </a>
+    </>
+  )
+}
