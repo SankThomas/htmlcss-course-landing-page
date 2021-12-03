a8528dcc907246ca03603b3de230422990ddc46c
Thomas Sankara <thomassankara74@gmail.com>
2021-12-03 12:33:58 +0300

    Course Discussion button

---
+++ src/buttons/CourseDiscussion.js
@@ -0,0 +1,14 @@
+export default function CourseDiscussion() {
+  return (
+    <>
+      <a
+        href="https://www.udemy.com/course/tsbdesigns-web-development-course/learn/lecture/25270392#questions"
+        target="_blank"
+        rel="noopenner noreferrer"
+        className="bg-blue-500 text-white font-bold py-2 px-6 rounded shadow border-2 border-blue-500 hover:bg-transparent hover:text-blue-500 duration-500 transition-all block"
+      >
+        Course discussion
+      </a>
+    </>
+  )
+}
